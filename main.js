0276435499ce57b96e722d490da4743dfff01437
l3qua <99524581+l3qua@users.noreply.github.com>
2023-08-30 19:07:31 +0700

    An Update to the Website
    There is an easter egg. Try to find it!

---
+++ main.js
@@ -0,0 +1,19 @@
+let date = "Aug 30 2023";
+const time = "08:50:15";
+const a = 8;
+const b = 5;
+
+function iuak(){
+    for(let i = 0; i < 10; i++){
+        console.log("yêu anh Kiệt!")
+        console.log("Yêu Anh Kiệt!!")
+        console.log("YÊU ANH KIỆT!!!")
+    }
+}
+
+const fl = () => {
+    console.log("Welcome to l3qua Website's Console");
+    console.log(`Last Update: ${date} ${time} UTC`);
+    console.log("DO NOT EXECUTE ANY COMMAND FROM A PERSON THAT YOU DON'T TRUST! DO A GOOGLE SEARCH BEFORE EXECUTE ANY COMMAND!");
+}
+fl()
