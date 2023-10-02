6a475a8d66ecbbf2aea9ca91cb9cb4ad80683caa
l3qua <99524581+l3qua@users.noreply.github.com>
2023-10-02 11:30:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ const ver = "Beta 0.2.10"
 const a = 8;
 const b = 5;
 
-function ver(){
+function ver{
     for(let i = 0; i < 10; i++){
         console.log("Current Website Version: ",ver)
     }
 }
 
-const fl = () => {
+const lu = () => {
     console.log("Welcome to l3qua Website's Console");
     console.log(`Last Update: ${date} ${time} UTC`);
     console.log("DO NOT EXECUTE ANY COMMAND FROM A PERSON THAT YOU DON'T TRUST! DO A GOOGLE SEARCH BEFORE EXECUTE ANY COMMAND!");
 }
-fl()
+lu()
 ver()
