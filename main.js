ae9f860b3c72d6c8c4e2feb7661b3e2344232ec6
l3qua <99524581+l3qua@users.noreply.github.com>
2023-10-02 11:31:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let date = "Aug 30 2023";
 const time = "04:05:15";
-const ver = "Beta 0.2.10"
+const version = "Beta 0.2.10"
 const a = 8;
 const b = 5;
 
 function ver{
     for(let i = 0; i < 10; i++){
-        console.log("Current Website Version: ",ver)
+        console.log("Current Website Version: ",version)
     }
 }
 
