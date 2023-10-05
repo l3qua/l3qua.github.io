2f553a86f997ff001d3e4240d3e580b384e0efdd
l3qua <99524581+l3qua@users.noreply.github.com>
2023-10-05 09:52:32 +0700

    Redegined the website (again)
    Thanks to @notHyperNexus

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 let date = "Aug 30 2023";
 const time = "04:05:15";
-const version = "Beta 0.2.10"
+const version = "Beta 0.2.12"
 const a = 8;
 const b = 5;
 
-function ver{
+function ver(){
     for(let i = 0; i < 10; i++){
         console.log("Current Website Version: ",version)
     }
 }
 
-const lu = () => {
+const fl = () => {
     console.log("Welcome to l3qua Website's Console");
     console.log(`Last Update: ${date} ${time} UTC`);
     console.log("DO NOT EXECUTE ANY COMMAND FROM A PERSON THAT YOU DON'T TRUST! DO A GOOGLE SEARCH BEFORE EXECUTE ANY COMMAND!");
 }
-lu()
+fl()
 ver()
