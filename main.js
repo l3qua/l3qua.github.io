c42423bbf85f3835697b5ebfea2639d14a42e795
l3qua <99524581+l3qua@users.noreply.github.com>
2024-01-30 01:32:15 +0000

    Update website to version Beta 0.56

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-let date = "Jan 16 2024";
+let date = "Jan 30 2024";
 const time = "01:30:35 AM";
-const version = "Beta 0.5"
+const version = "Beta 0.56"
 const games = document.getElementById('games');
 const light = document.getElementById('light');
 
 function ver(){
         console.log("Current Website Version: ",version)
-}
-const fl = () => {
-    console.log("Welcome to l3qua Website's Console");
-    console.log(`Last Update: ${date} ${time} UTC`);
-    console.log("DO NOT EXECUTE ANY COMMAND FROM A PERSON THAT YOU DON'T TRUST! DO A GOOGLE SEARCH BEFORE EXECUTE ANY COMMAND!");
 }
 
 games.addEventListener('mousemove', e => {
@@ -26,4 +21,6 @@ games.addEventListener('mouseleave', () => {
 });
 
 ver()
-fl()
+console.log("Welcome to l3qua Website's Console");
+console.log(`Last Update: ${date} ${time} UTC`);
+console.log("DO NOT EXECUTE ANY COMMAND FROM A PERSON THAT YOU DON'T TRUST! DO A GOOGLE SEARCH BEFORE EXECUTE ANY COMMAND!");
--- stuffs/main.js
+++ stuffs/main.js
@@ -1,14 +1,14 @@
-const games = document.getElementById('peojects');
+let date = "Jan 30 2024";
+const time = "01:30:35 AM";
+const version = "Beta 0.56"
+const games = document.getElementById('games');
 const light = document.getElementById('light');
 
-games.addEventListener('mousemove', e => {
-    const rect = games.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    light.style.setProperty('--x', `${x}px`);
-    light.style.setProperty('--y', `${y}px`);
-});
-games.addEventListener('mouseleave', () => {
-    light.style.setProperty('--x', `-100%`);
-    light.style.setProperty('--y', `-100%`);
-});
+function ver(){
+        console.log("Current Website Version: ",version)
+}
+
+ver()
+console.log("Welcome to l3qua Website's Console");
+console.log(`Last Update: ${date} ${time} UTC`);
+console.log("DO NOT EXECUTE ANY COMMAND FROM A PERSON THAT YOU DON'T TRUST! DO A GOOGLE SEARCH BEFORE EXECUTE ANY COMMAND!");
