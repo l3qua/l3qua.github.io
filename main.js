137001be4bba6c3ad72d9d5018c983fc61499164
l3qua <99524581+l3qua@users.noreply.github.com>
2024-05-02 03:04:25 +0000

    Update the website to version "Beta 0.60"

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 let date = "Mar 1 2024";
+let docTitle = document.title;
 const time = "03:05 PM";
-const version = "Beta 0.56.1"
+const version = "Beta 0.60"
 const games = document.getElementById('games');
+const stuffs = document.getElementById('stuffs');
 const light = document.getElementById('light');
 
-function ver(){
-    console.log("Current Website Version: ",version)
-}
-
+try{ // Use try here so it will not broke the console on the "stuffs" site
 games.addEventListener('mousemove', e => {
     const rect = games.getBoundingClientRect();
     const x = e.clientX - rect.left;
@@ -18,9 +17,16 @@ games.addEventListener('mousemove', e => {
 games.addEventListener('mouseleave', () => {
     light.style.setProperty('--x', `-100%`);
     light.style.setProperty('--y', `-100%`);
-});
+}); } catch {} // Have to use "catch" when using "try"
 
-ver()
+window.addEventListener("blur", () => {
+    document.title = "Bro Come Backkk ☹️"
+})
+window.addEventListener("focus", () => {
+    document.title = docTitle
+})
+
+console.log("Current Website Version: ",version)
 console.log("Welcome to l3qua Website's Console");
 console.log(`Last Update: ${date} ${time} UTC`);
 console.log("DO NOT EXECUTE ANY COMMAND FROM A PERSON THAT YOU DON'T TRUST! DO A GOOGLE SEARCH BEFORE EXECUTE ANY COMMAND!");
