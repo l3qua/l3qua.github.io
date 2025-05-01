8d474c0e05e3a0de6815101ef8f6d1c5168a3b5c
Quang Le <auq3l.450@gmail.com>
2025-05-01 19:33:02 +0700

    fix code to allows icons to render from anchient browsers (Tested with Opera Mini v4.4 on Nokia 105 4G 2021)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,11 @@
 let docTitle = document.title; // For line 25 to work propely
-let date = "Jan 10 2025"; // Site deployment date
-const time = "12:15 PM"; // Site deployment time
-const version = "Beta 0.85.0" // a few more version before stable
+let date = "May 1 2025"; // Site deployment date
+const time = "19:35 PM"; // Site deployment time
+const version = "Beta 0.85.2" // a few more version before stable
 const projects = document.getElementById('projects'); // For main site
 const light = document.getElementById('light'); // For "try" event at line 8
 
-try{ // Use try here so it will not broke the console on the "stuffs" site
+try{ // Use try here so it will not break the console on the "stuffs" site
 projects.addEventListener('mousemove', e => {
     const rect = projects.getBoundingClientRect();
     const x = e.clientX - rect.left;
@@ -28,6 +28,6 @@ window.addEventListener("focus", () => {
 // Some useless line when console started
 console.log("Current Website Version: ",version)
 console.log("Welcome to l3qua Website's Console");
-console.log(`Last Update: ${date} ${time} UTC`);
-console.log("NEVER EXECUTE ANY COMMAND FROM A PERSON THAT YOU DON'T TRUST! DO A GOOGLE SEARCH BEFORE EXECUTE ANY COMMAND!");
+console.log(`Last Update: ${date} ${time} UTC+7`);
+// console.log("NEVER EXECUTE ANY COMMAND FROM A PERSON THAT YOU DON'T TRUST! DO A GOOGLE SEARCH BEFORE EXECUTE ANY COMMAND!");
 // End of those useless lines and script file
